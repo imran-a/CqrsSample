0b3ff2d1d33388c5b56cf70c1b1f77c4828a29e9
Imran Arshed <imran@shoot.com>
2014-08-01 17:33:00 +0100

    added basic action

--- CqrsSample/Controllers/HomeController.cs
+++ CqrsSample/Controllers/HomeController.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CqrsSample.Domain;
+using CqrsSample.Infrastructure;
 
 namespace CqrsSample.Controllers
 {
     public class HomeController : Controller
     {
+
+        public HomeController()
+        {
+        }
+
         public ActionResult Index()
         {
+            var connection = Configuration.CreateConnection();
+            var domainRepository = new EventStoreDomainRepository(connection);
+            var application = new DomainEntry(domainRepository);
+
+
+            var userCommand = new CreateUser(new Guid(), "first", "surname");
+
+            application.ExecuteCommand(userCommand);
+
             return View();
         }
 
